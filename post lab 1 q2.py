c6271abc3c03078c78d46ac99659241852f640e9
pateldhananjay39 <dhananjaysavsani@gmail.com>
2025-09-07 19:00:56 +0530

    Add files via upload

---
+++ post lab 1 q2.py
@@ -0,0 +1,5 @@
+
+
+print("a     a*2     a*3")
+for a in range(1, 5):
+    print(f"{a:<5}{a**2:<7}{a**3}")
---
+++ post lab 1 q3.py
@@ -0,0 +1,5 @@
+numerator = 9.5 * 4.5 - 2.5 * 3
+denominator = 45.5 - 3.5
+
+result = numerator / denominator
+print("The result is:", result)
